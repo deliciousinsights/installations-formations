af8c3e03ef80c57edb3db9eb4aca02e759d18bee
Christophe Porteneuve <christophe@delicious-insights.com>
2022-04-05 00:26:30 +0200

    Initial import (v2)

---
+++ docs/courses/wam.md
@@ -0,0 +1,18 @@
+---
+sidebar_position: 1
+---
+
+# Web Apps Modernes
+
+Ah, notre **best-seller**… [Un vrai programme de rêve](https://delicious-insights.com/fr/formations/web-app-modernes/#program)…
+
+## Installations obligatoires
+
+Il te faudra :
+
+1. **[Git](/docs/software/git)**, pour récupérer la base de **code de départ** et pouvoir **naviguer entre les étapes** si tu décides de ne pas toutes les faire toi-même, si tu es trop empêtré-e dans une d’elles et veux t’en sortir en un clin d’œil, ou simplement pour récupérer du code de début d’étape ou d’exercice.
+2. **[Node & npm](/docs/software/node)**, pour **gérer et installer les dépendances** à de multiples outils, modules et bibliothèques JS dont tu auras besoin (par exemple, Webpack, React, Redux et Babel, pour ne citer qu’eux).
+
+## Installations recommandées
+
+1. **[Visual Studio Code et des extensions](/docs/software/vscode)**, pour éditer à la vitesse du son, naviguer facilement entre les fichiers du projet, bénéficier d’une **coloration à la pointe** et d’une **complétion** qualitative sur ta _codebase_, et plus encore !
---
+++ docusaurus.config.js
@@ -0,0 +1,133 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Installations Delicious Insights',
+  tagline: 'Histoire d’arriver préparé·e',
+  url: 'https://installations.delicious-insights.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'deliciousinsights', // Usually your GitHub org/user name.
+  projectName: 'installations-formations', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/deliciousinsights/installations-formations/tree/v2',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Installations',
+        logo: {
+          alt: 'Installations Delicious Insights',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'courses/wam',
+            position: 'left',
+            label: 'Formations',
+          },
+          {
+            type: 'doc',
+            docId: 'software/git',
+            position: 'left',
+            label: 'Logiciels',
+          },
+          {
+            href: 'https://github.com/deliciousinsights/installations-formations',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Formations',
+                to: '/docs/courses/wam',
+              },
+              {
+                label: 'Logiciels',
+                to: '/docs/software/git',
+              },
+              {
+                label: 'JS idiomatique',
+                to: 'https://www.youtube.com/playlist?list=PLPoAfTkDs_Jad8tGdMkixD1r7lMjpRstB',
+              },
+              {
+                label: 'Les concepts clés de Git',
+                to: 'https://www.youtube.com/playlist?list=PLPoAfTkDs_JbMPY-BXzud0aMhKKhcstRc',
+              },
+            ],
+          },
+          {
+            title: 'Communauté',
+            items: [
+              {
+                label: 'Site officiel',
+                href: 'https://delicious-insights.com',
+              },
+              {
+                label: 'Chaîne YouTube',
+                href: 'https://youtube/c/DeliciousInsights',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/DelicioInsights',
+              },
+            ],
+          },
+          {
+            title: 'Explorer',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/deliciousinsights',
+              },
+              {
+                label: 'Articles',
+                href: 'https://delicious-insights.com/fr/archives/#articles',
+              },
+              {
+                label: 'Conférences / Talks / Meetups',
+                href: 'https://www.youtube.com/playlist?list=PLPoAfTkDs_JZ-bQhbohZYXhMdFWDt3QCJ',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © 2011–${new Date().getFullYear()} Delicious Insights SAS. Propulsé par Docusaurus. Écrit avec ❤️.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+}
+
+module.exports = config
